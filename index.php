6e38ddfd74bc0289bf1f4a12e2d773a9ab3941b0
Farhanunnasih <farhanunnasih@gmail.com>
2022-01-22 23:24:06 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Education</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+
+	<!-- Pricing -->
+	<link rel="stylesheet" href="css/pricing.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-4">
+						<div id="fh5co-logo">
+							<a href="index.html">
+								<img src="images/logo-resized.png" alt="">
+								Bina Dharma
+							</a>
+						</div>
+					</div>
+					<div class="col-xs-8 text-right menu-1" style="margin-top: 1.5vw;">
+						<ul id="nav-menu">
+							<li class="active"><a href="index.html">Home</a></li>
+							<li><a href="#about">About</a></li>
+							<li><a href="#testimonial">Testimonials</a></li>
+							<li><a href="#blog">Blog</a></li>
+							<li class="btn-cta"><a href="#tentang"><span>Hubungi Kami</span></a></li>
+						</ul>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</nav>
+	
+	<aside id="fh5co-hero">
+		<div class="flexslider">
+			<ul class="slides">
+		   	<li style="background-image: url(images/Banner_crop.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>Unggul Dalam Prestasi, Disiplin, Bersih, dan Hijau Berlandaskan Iman dan Taqwa</h1>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>
+		   	<!-- <li style="background-image: url(images/img_bg_2.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>The Great Aim of Education is not Knowledge, But Action</h1>
+									<h2>Brought to you by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+									<p><a class="btn btn-primary btn-lg btn-learn" href="#">Start Learning Now!</a></p>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>
+		   	<li style="background-image: url(images/img_bg_3.jpg);">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="row">
+			   			<div class="col-md-8 col-md-offset-2 text-center slider-text">
+			   				<div class="slider-text-inner">
+			   					<h1>We Help You to Learn New Things</h1>
+									<h2>Brought to you by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+									<p><a class="btn btn-primary btn-lg btn-learn" href="#">Start Learning Now!</a></p>
+			   				</div>
+			   			</div>
+			   		</div>
+		   		</div>
+		   	</li>		   	 -->
+		  	</ul>
+	  	</div>
+	</aside>
+
+	<div id="fh5co-counter" class="fh5co-counters" style="background-image: url(images/img_bg_4.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="row">
+						<div class="col-md-3 col-sm-6 text-center animate-box">
+							<span class="icon"><i class="icon-home"></i></span>
+							<span class="fh5co-counter js-counter" data-from="0" data-to="25" data-speed="5000" data-refresh-interval="50"></span>
+							<span class="fh5co-counter-label">Kelas</span>
+						</div>
+						<div class="col-md-3 col-sm-6 text-center animate-box">
+							<span class="icon"><i class="icon-head"></i></span>
+							<span class="fh5co-counter js-counter" data-from="0" data-to="28" data-speed="5000" data-refresh-interval="50"></span>
+							<span class="fh5co-counter-label">Guru Tersertifikasi</span>
+						</div>
+						<div class="col-md-3 col-sm-6 text-center animate-box">
+							<span class="icon"><i class="icon-study"></i></span>
+							<span class="fh5co-counter js-counter" data-from="0" data-to="528" data-speed="5000" data-refresh-interval="50"></span>
+							<span class="fh5co-counter-label">Siswa</span>
+						</div>
+						<div class="col-md-3 col-sm-6 text-center animate-box">
+							<span class="icon"><i class="icon-bulb"></i></span>
+							<span class="fh5co-counter-label akre-label">Akreditasi</span>
+							<span class=" akre">A</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-course" style="background-image: url(images/vm_dudes.jpg);">
+        <div class="overlays"></div>
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading2">
+					<h2 id="about">Visi</h2>
+					<h4>Unggul Dalam Prestasi, Disiplin, Bersih, dan Hijau Berlandaskan Iman dan Taqwa</h4>
+				</div>
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading2">
+					<h2>Misi</h2>
+					<h4>
+						<ul>
+							<li>1. Mengembangkan sumber daya secara optimal dalam rangka mempersiapkan peserta didik di era global</li>
+							<li>2. Menumbuhkan proses belajar mengajar yang aktif, inovatif, kreatif dan menyenangkan</li>
+							<li>3. Meningkatkan sumber daya manusia yangberkepribadian unggul dan berwawasan lingkungan</li>
+							<li>4. Meningkatkan kegiatan ekstrakurikuler untuk mengembangkan minat dan bakat peserta didik dalam meraih prestasi</li>
+							<li>5. Mengembangkan kegiatan sosal dan keagamaan</li>
+						</ul>
+					</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-testimonial" style="background-image: url(images/Court.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2 id="testimonial"><span>Testimonials</span></h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<div class="row animate-box">
+						<div class="owl-carousel owl-carousel-fullwidth">
+							<div class="item">
+								<div class="testimony-slide active text-center">
+									<div class="user" style="background-image: url(images/person1.jpg);"></div>
+									<span>Ade Roni<br><small>Murid</small></span>
+									<blockquote>
+										<p>&ldquo;Saya pernah bersekolah di SMP Bina Dharma 2 Bandung Tahun 2014-2017. Bersyukur bisa bersekolah di yayasan pendidikan  yang sangat berkualitas dengan bekal ilmu agama serta pembentukan penguatan iman bagi siswa/siswi dengan para pengajar tersertifikasi. Lulus dari sekolah ini, alhamdullilah saya mampu bersaing di SMA Negeri dan PTN untuk S1 dan S2. Terimakasih SMP SMP Bina Dharma 2 telah menjadi pintu awal bagi saya untuk menjadi pribadi yang beriman di dalam pengabdian saya kepada NKRI..&rdquo;</p>
+									</blockquote>
+								</div>
+							</div>
+							<div class="item">
+								<div class="testimony-slide active text-center">
+									<div class="user" style="background-image: url(images/person2.jpg);"></div>
+									<span>Arif Firmansyah, Alumnus Tahun 2003<br><small>Murid</small></span>
+									<blockquote>
+										<p>&ldquo;Bina Dharma merupakan sekolah yang tepat bagi saya untuk mendapat pengetahuan dan yang membina karakter sehingga saya menjadi pribadi yang taat akan Tuhan..&rdquo;</p>
+									</blockquote>
+								</div>
+							</div>
+							<div class="item">
+								<div class="testimony-slide active text-center">
+									<div class="user" style="background-image: url(images/person3.jpg);"></div>
+									<span>Pramudya Arya Wicaksana<br><small>Murid</small></span>
+									<blockquote>
+										<p>&ldquo;Sebuah tempat dimana saya belajar untuk tidak hanya menjadi unggul dalam bidang akademik tetapi memiliki nilai hidup yaitu keberanian untuk memilih. So, don’t be afraid!&rdquo;</p>
+									</blockquote>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-blog">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2 id="blog">Blog &amp; Events</h2>	
+				</div>
+				
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						<a href="#" class="blog-img-holder" style="background-image: url(images/project-1.jpg);"></a>
+						<div class="blog-text">
+							<h3><a href="#">Jawa Barat, Lomba Seni 2015</a></h3>
+							<span class="posted_on">15 Maret</span>
+							<span class="comment"><a href="">41<i class="icon-speech-bubble"></i></a></span>
+							<p>Siswa yang bernama Irfan Firmansyah alhamdullilah berhasil meraih peringkat 1 di ajan...</p>
+						</div> 
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						<a href="#" class="blog-img-holder" style="background-image: url(images/project-2.jpg);"></a>
+						<div class="blog-text">
+							<h3><a href="#">Vaksinasi Covid Bina Dharma 2 Bandung</a></h3>
+							<span class="posted_on">21 November</span>
+							<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
+							<p>Bina Dharma mengadakan vaksin massal bagi para siswa yang hendak me...</p>
+						</div> 
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-gallery" class="fh5co-bg-section">
+		<div class="row text-center">
+			<h2 id="galeri"><span>Galeri</span></h2>
+		</div>
+		<div class="row">
+			<div class="col-md-3 col-padded">
+				<a href="#" class="gallery" style="background-image: url(images/project-5.jpg);"></a>
+			</div>
+			<div class="col-md-3 col-padded">
+				<a href="#" class="gallery" style="background-image: url(images/project-2.jpg);"></a>
+			</div>
+			<div class="col-md-3 col-padded">
+				<a href="#" class="gallery" style="background-image: url(images/project-3.jpg);"></a>
+			</div>
+			<div class="col-md-3 col-padded">
+				<a href="#" class="gallery" style="background-image: url(images/project-4.jpg);"></a>
+			</div>
+		</div>
+	</div>
+
+	<footer id="fh5co-footer" role="contentinfo" style="background-image: url(images/img_bg_4.jpg);">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-3 fh5co-widget">
+					<h3 id="tentang">Tentang Edukasi</h3>
+					<p>Education is not preparation for life; education is life itself. – <strong>John Dewey</strong></p>
+				</div>
+				<div class="col-md-4 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
+					<h3>Contact</h3>
+					<ul class="fh5co-footer-link">
+						<li>
+							<span class="icon"><i class="icon-phone"></i> 0227272862</span>
+						</li>
+						<li>
+							<span class="icon"><i class="icon-home"></i> Jl. Babakan Sari 1 No. 131 | Kec. Kiaracondong</span>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-2 col-sm-4 col-xs-6 col-md-push-1 fh5co-widget">
+					<h3>Learning</h3>
+					<img id="maps" src="images/map.png" alt="">
+				</div>
+
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2021.</small> 
+						<small class="block">By <a href="#" target="_blank">Farhanunnasih</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a> &amp; <a href="https://www.pexels.com/" target="_blank">Pexels</a></small>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Count Down -->
+	<script src="js/simplyCountdown.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+	<script>
+    var d = new Date(new Date().getTime() + 1000 * 120 * 120 * 2000);
+
+    // default example
+    simplyCountdown('.simply-countdown-one', {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate()
+    });
+
+    //jQuery example
+    $('#simply-countdown-losange').simplyCountdown({
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        enableUtc: false
+	});
+	
+	document.getElementById("akreditasi").innerHTML = "A";
+	
+	</script>
+	</body>
+</html>
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Website SMP Bina Dharma 2 Bandung
+## UAS Web Programming
+### Dumped all files with haste, expect low-quality files organization.
